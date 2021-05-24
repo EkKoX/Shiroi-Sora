5b9f32dca61c3ae0fd02008167ace7806a50b343
EkKoX <53996869+EkKoX@users.noreply.github.com>
2021-05-24 09:07:06 +0200

    Add files via upload

--- cambiarTema.js
+++ cambiarTema.js
@@ -9,62 +9,65 @@ const cambiarArma = document.querySelector(".arma");
 const cambiarPocion = document.querySelector(".pocion");
 const cambiarGranada = document.querySelector(".granada");
 var ancho = window.matchMedia("(min-width: 860px)");
+const page = document.getElementById("page");
 
 botonCambiarTema.addEventListener("change", function(){
     if(this.checked){
-        document.documentElement.setAttribute("tema", "noche")
-            cambioIcono.classList.remove("fa-moon");
-            cambioIcono.classList.add("fa-sun");
-            if(ancho.matches){
-            cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu2.png')";
-            } else {
-                cambiarFondo.style.backgroundImage = "none";
-            }
-            if(cambiarArma != null){
-                cambiarArma.setAttribute("src", "imagenes/logoarma2.png")
-            }
-            if(cambiarCasco != null){
-                cambiarCasco.setAttribute("src", "imagenes/logopechera2.png")
-            }
-            if(cambiarGrebas != null){
-                cambiarGrebas.setAttribute("src", "imagenes/logogrebas2.png")
-            }
-            if(cambiarPechera != null){
-                cambiarPechera.setAttribute("src", "imagenes/logocascos2.png")
-            }
-            if(cambiarGranada != null){
-                cambiarGranada.setAttribute("src", "imagenes/granada2.png")
-            }
-            if(cambiarPocion != null){
-                cambiarPocion.setAttribute("src", "imagenes/pocion2.png")
-            }
+        page.classList.toggle("dia");
+        page.classList.toggle("noche");
+        cambioIcono.classList.remove("fa-moon");
+        cambioIcono.classList.add("fa-sun");
+        if(ancho.matches){
+        cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu2.png')";
+        } else {
+            cambiarFondo.style.backgroundImage = "none";
+        }
+        if(cambiarArma != null){
+            cambiarArma.setAttribute("src", "imagenes/logoarma2.png")
+        }
+        if(cambiarCasco != null){
+            cambiarCasco.setAttribute("src", "imagenes/logopechera2.png")
+        }
+        if(cambiarGrebas != null){
+            cambiarGrebas.setAttribute("src", "imagenes/logogrebas2.png")
+        }
+        if(cambiarPechera != null){
+            cambiarPechera.setAttribute("src", "imagenes/logocascos2.png")
+        }
+        if(cambiarGranada != null){
+            cambiarGranada.setAttribute("src", "imagenes/granada2.png")
+        }
+        if(cambiarPocion != null){
+            cambiarPocion.setAttribute("src", "imagenes/pocion2.png")
+        }
     }else{
-        document.documentElement.setAttribute("tema", "dia");
-            cambioIcono.classList.remove("fa-sun");
-            cambioIcono.classList.add("fa-moon");
-            if(ancho.matches){
-                cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu.jpg')";
-            } else {
-                cambiarFondo.style.backgroundImage = "none";
-            }
-            if(cambiarArma != null){
-                cambiarArma.setAttribute("src", "imagenes/logoarma.png")
-            }
-            if(cambiarCasco != null){
-                cambiarCasco.setAttribute("src", "imagenes/logopechera.png")
-            }
-            if(cambiarGrebas != null){
-                cambiarGrebas.setAttribute("src", "imagenes/logogrebas.png")
-            }
-            if(cambiarPechera != null){
-                cambiarPechera.setAttribute("src", "imagenes/logocascos.png")
-            }
-            if(cambiarGranada != null){
-                cambiarGranada.setAttribute("src", "imagenes/granada.png")
-            }
-            if(cambiarPocion != null){
-                cambiarPocion.setAttribute("src", "imagenes/pocion.png")
-            }
+        page.classList.toggle("dia");
+        page.classList.toggle("noche");
+        cambioIcono.classList.remove("fa-sun");
+        cambioIcono.classList.add("fa-moon");
+        if(ancho.matches){
+            cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu.jpg')";
+        } else {
+            cambiarFondo.style.backgroundImage = "none";
+        }
+        if(cambiarArma != null){
+            cambiarArma.setAttribute("src", "imagenes/logoarma.png")
+        }
+        if(cambiarCasco != null){
+            cambiarCasco.setAttribute("src", "imagenes/logopechera.png")
+        }
+        if(cambiarGrebas != null){
+            cambiarGrebas.setAttribute("src", "imagenes/logogrebas.png")
+        }
+        if(cambiarPechera != null){
+            cambiarPechera.setAttribute("src", "imagenes/logocascos.png")
+        }
+        if(cambiarGranada != null){
+            cambiarGranada.setAttribute("src", "imagenes/granada.png")
+        }
+        if(cambiarPocion != null){
+            cambiarPocion.setAttribute("src", "imagenes/pocion.png")
+        }
     }
 })
 function boton(){
--- formulario.js
+++ formulario.js
@@ -6,21 +6,23 @@ var ancho = window.matchMedia("(min-width: 860px)");
 
 botonCambiarTema.addEventListener("change", function(){
     if(this.checked){
-        document.documentElement.setAttribute("tema", "noche")
-            cambioIcono.classList.remove("fa-moon");
-            cambioIcono.classList.add("fa-sun");
-            if(ancho.matches){
-            cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu2.png')";
-            }
-            cambioFondo.style.backgroundImage = "url('imagenes/fondoiniciarsesion2.jpg')";
+        page.classList.toggle("dia");
+        page.classList.toggle("noche");
+        cambioIcono.classList.remove("fa-moon");
+        cambioIcono.classList.add("fa-sun");
+        if(ancho.matches){
+        cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu2.png')";
+        }
+        cambioFondo.style.backgroundImage = "url('imagenes/fondoiniciarsesion2.jpg')";
     }else{
-        document.documentElement.setAttribute("tema", "dia");
-            cambioIcono.classList.remove("fa-sun");
-            cambioIcono.classList.add("fa-moon");
-            if(ancho.matches){
-                cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu.jpg')";
-            }
-            cambioFondo.style.backgroundImage = "url('imagenes/fondoiniciarsesion.jpg')";
+        page.classList.toggle("dia");
+        page.classList.toggle("noche");
+        cambioIcono.classList.remove("fa-sun");
+        cambioIcono.classList.add("fa-moon");
+        if(ancho.matches){
+            cambiarFondo.style.backgroundImage = "url('imagenes/fondomenu.jpg')";
+        }
+        cambioFondo.style.backgroundImage = "url('imagenes/fondoiniciarsesion.jpg')";
     }
 })
 function boton(){
