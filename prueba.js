3af46eb55143cf7e355673ef80d33eb4613aede6
EkKoX <53996869+EkKoX@users.noreply.github.com>
2021-05-30 14:21:51 +0200

    Add files via upload

--- prueba.js
+++ prueba.js
@@ -1,6 +1,11 @@
-var button = document.getElementById('button');
 var nav = document.getElementById('nav');
+var navBg = document.getElementById('navBg');
 
 function activateShow() {
-    nav.classList.toggle("show")
-  }
+  nav.classList.toggle("show");
+  navBg.classList.toggle("show");
+}
+navBg.onclick = function(){
+  nav.classList.toggle("show");
+  navBg.classList.toggle("show");
+};
